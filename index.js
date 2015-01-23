4436b380a296fe7f38a78787419ce93c82476549
kenji-special <k.saito.1985@gmail.com>
2015-01-23 17:45:01 +0900

    update canvas-bako

--- index.js
+++ index.js
@@ -1,29 +1,55 @@
 var addEvent = require("add-event-listener");
 
-var defaultCanvasValue = {
-    id : 'c',
-    width : window.innerWidth,
-    high  : window.innerHeight
-};
+var CanvasBako = function(loop){
+    this.canvas = document.createElement("canvas");
+    this.ctx = this.canvas.getContext("2d");
 
-var CanvasBako = function(_vars){
-    var vars = _vars || defaultCanvasValue;
+    this.width = window.innerWidth;
+    this.height = window.innerHeight;
 
-    if(vars.id)     this.canvas = document.getElementById(vars.id);
-    else            this.canvas = document.getElementById(defaultCanvasValue.id);
+    this.canvas.width  = this.width;
+    this.canvas.height = this.height;
 
-    if(vars.width)  this.canvas.width  = vars.width;
-    else            this.canvas.width  = defaultCanvasValue.width;
+    this.prev = Date.now();
 
-    if(vars.height) this.canvas.height = vars.height;
-    else            this.canvas.height = defaultCanvasValue.height;
+    if(typeof loop === "function"){
+        this.onLoop = loop.bind(this);
+    } else {
+        this.onLoop = function(ctx, dt){
+            console.log("nothing rendered");
+        }
+    }
 
+    this.renderOnce = function(){
+        var now, dt;
 
-    this._renderHandler = function(){
-        
+        now = Date.now();
+        dt = (now - this.prev) / 1000;
+
+        this.onLoop(this.ctx, dt);
+
+        this.prev = now;
     };
 
+
+    this._renderHanlder = function(){
+        this.renderOnce();
+
+        requestAnimationFrame(this._renderHanlder);
+    }.bind(this);
+
 };
+
+CanvasBako.prototype.start = function(){
+
+    this.lastFrame = requestAnimationFrame(this._renderHanlder);
+};
+
+CanvasBako.prototype.stop = function(){
+    cancelRequestAnimationFrame(this.lastFrame);
+}
+
+
 
 
 
